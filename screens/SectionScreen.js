97c06af4c4d4b07614f59b6b982abb0333084439
ataricoder <ataricoder@gmail.com>
2019-09-06 12:31:27 -0500

    edited section screens

--- screens/SectionScreen.js
+++ screens/SectionScreen.js
@@ -3,18 +3,25 @@ import styled from "styled-components";
 import { Button } from "react-native";
 
 class SectionScreen extends React.Component {
-  static navigationOptions = {};
+  static navigationOptions = {
+    title: "Booking"
+  };
 
   render() {
     return (
       <Container>
-        <Text>Section Screen</Text>
-        <Button
-          title="Close"
-          onPress={() => {
-            this.props.navigation.goBack();
-          }}
-        />
+        <Content>
+          <Text>Monday, September 9</Text>
+          <Text2>Home Cleaning</Text2>
+          <Text3>1 Bed/1 Bath</Text3>
+          <Text4>2100 Rio Grande St, Austin, TX 78705. Apt 319</Text4>
+          <Edit>
+            <Text5>Edit</Text5>
+          </Edit>
+          <Cancel>
+            <Text6>Cancel</Text6>
+          </Cancel>
+        </Content>
       </Container>
     );
   }
@@ -22,10 +29,73 @@ class SectionScreen extends React.Component {
 
 export default SectionScreen;
 
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+const Text5 = styled.Text`
+  width: 100%;
+  text-align: center;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 500;
 `;
 
-const Text = styled.Text``;
+const Text6 = styled.Text`
+  width: 100%;
+  text-align: center;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+const Edit = styled.View`
+  width: 335px;
+  height: 60px;
+  background-color: #4abbed;
+  margin-top: 40px;
+  border-radius: 14px;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
+  justify-content: center;
+`;
+
+const Cancel = styled.View`
+  width: 335px;
+  height: 60px;
+  background-color: #ff5b82;
+  margin-top: 20px;
+  border-radius: 14px;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
+  justify-content: center;
+`;
+
+const Content = styled.View``;
+
+const Container = styled.View`
+  flex: 1;
+  padding: 20px;
+  background-color: #fff;
+`;
+
+const Text = styled.Text`
+  font-size: 28px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const Text2 = styled.Text`
+  font-size: 24px;
+  color: #3c4560;
+  font-weight: 500;
+  padding-top: 20px;
+`;
+
+const Text3 = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: 500;
+  padding-top: 20px;
+`;
+
+const Text4 = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: 500;
+  padding-top: 20px;
+`;
